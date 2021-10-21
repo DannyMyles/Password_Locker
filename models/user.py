f95deaeff23bdbd5d3d5cdb30803a84ea353dab2
Moringa <dancun.wawire@student.moringaschool.com>
2021-10-21 18:33:51 +0300

    created user model

--- models/user.py
+++ models/user.py
@@ -1,3 +1,10 @@
+from _typeshed import Self
+
+
 class User:
-    def _init_(name,password, email,username):
-        self.name
+    def _init_(self,name,password, email,username):
+        self.name = name
+        self.password =password
+        self.email = email
+        self.username =username
+
