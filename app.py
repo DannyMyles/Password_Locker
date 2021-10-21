ea58f0c3ca4b5f6da79a0a9e67fadbfdb07b2af2
Moringa <dancun.wawire@student.moringaschool.com>
2021-10-21 19:47:43 +0300

    added while loop

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from models.user import User
-
+from getpass import getpass
 
 credentials = []
 user = None
@@ -7,9 +7,14 @@ def signUp():
     name= input("Enter your fullname :")
     email=input("Enter your email :")
     username= input("Enter your username :")
-    password=input("Enter your password :")
+    password=getpass("Enter your password :")
     print("welcome "+name)
 
 if __name__ ==  "__main__":
-    if user is None:
-        signUp()
+    while True:
+        try:
+            if user is None:
+                signUp()
+
+        except KeyboardInterrupt:
+            print()
