b9c30005a5d0a8dfc8e6bb1a0060dd934234bbd0
Moringa <dancun.wawire@student.moringaschool.com>
2021-10-26 16:28:43 +0300

    last commit

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 ***DannyMyles***
 
 ## Description
-This is a python application that manages Log-in and Sign-up credentials of a person for various accounts that is the Usernames and Passwords for each account. It also stores the passwords and generates a unique password for a user if they do not want to generate new passwords by themselves.
+This is a python application that manages Login and Signup credentials of a person for various accounts that is the Usernames and Passwords for each account. It also stores the passwords and generates a unique password for a user if they do not want to generate new passwords by themselves.
 
 ## Set-up instructions
 ### Install
--- models/user.py
+++ models/user.py
@@ -1,30 +1,54 @@
-
-allUsers = []
-
-
 class User:
-    def __init__(self,name, username,email,password):
-        self.name = name
-        self.email = email
-        self.username =username
-        self.password =password
+    """
+    Class that generates new instances of user
+    """
 
-class Users:
-   def signIn(self,username,password):
-       for user in allUsers:
-           if(user.username == username and user.password == password):
-               return user
+    User_list = [] # Empty user list
 
-       return None
+    def __init__(self,username, password): #method that helps us define properties for our objects.
 
-   def signUp(self,user,password):
-        print(user)
-        for usr in allUsers :
-            if user.email==usr.email :
-                return "emial already in use"
-            if user.username==usr.username :
-                return "username already in use"
-        newUser = User(user.name, user.username, user.email, password)
-        allUsers.append(newUser)
+        self.username = username
+        self.password = password
 
-        return "User created successfully wecome"+newUser.name
+    def save_user(self):
+        '''
+        save new user to the application
+        '''
+        User.user_list.append(self)
+
+    def delete_user(self):
+
+        '''
+        delete_user method deletes a saved user from the user_list
+        '''
+
+    @classmethod
+    def display_all_users(cls):
+        '''
+        method that returns the user list
+        '''
+        return cls.user_list
+
+    @classmethod
+    def find_by_username(cls,username):
+        '''
+        Method that takes in a username and returns a user that matches that username.
+        Args:
+            username: user to search for
+        Returns :
+            user  that matches the username.
+        '''
+
+        for user in cls.user_list:
+            if user.username == username:
+                return user
+
+    @classmethod
+    def user_exist(cls,username):
+        '''
+        checks if a given user exists
+        '''
+
+        for contact in cls.contact_list:
+            if contact.phone_number == number:
+                return contact
